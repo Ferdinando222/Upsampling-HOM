0c9ef816cc4d19b721d1469957d8c64754e8f65d
Ferdinando222 <71137846+Ferdinando222@users.noreply.github.com>
2024-04-10 12:31:03 +0200

    Little modifications

--- src/test.py
+++ src/test.py
@@ -1,6 +1,5 @@
 # %%
 import torch
-import torch.nn as nn
 import fnn
 import global_variables as gb
 import pandas as pd
@@ -36,7 +35,7 @@ model = fnn.PINN(gb.input_dim,gb.output_dim,512,4,1,5,5).to(gb.device)
 model.load_state_dict(torch.load(path_saving))
 model.eval()
 
-## PRINT RESULTS
+#-------------------------------------------------- PRINT RESULTS--------------------------------------------------------
 
 # Make previsions
 input_data = data_handler.X_data.to(gb.device)
@@ -47,13 +46,11 @@ prevision_freq = previsions_pinn
 fft_result_full = np.concatenate((previsions_pinn, np.conj(np.fliplr(previsions_pinn[:, 1:]))), axis=1)
 previsions_time = np.real(np.fft.ifft(fft_result_full))
 
-
 # Compute NMSE in time for each channel and NMSE in frequency
 drir_ref = data_handler.drir
 drir_ref = torch.tensor(drir_ref,dtype=torch.float32)
 drir_prev = torch.tensor(previsions_time,dtype=torch.float32)
 mean_nmse_time,nmse_time,std_dev= lf.NMSE(drir_ref,drir_prev)
-
 diff = np.abs(drir_ref.cpu().numpy()-previsions_time)
 
 print("NMSE TIME:",mean_nmse_time,std_dev)
@@ -88,30 +85,24 @@ utils.plot_model(data_handler,previsions_pinn,points_sampled,pinn,index)
 #%%
 # Plot average NMSE for each channel
 plt.figure(4)
-
 az = data_handler.azimuth
 col = data_handler.colatitude
-
 fig, (ax,ax1) = plt.subplots(1, 2, figsize=(12, 5))
-# Create scatter plot
 
+# Create scatter plot
 sc = ax.scatter(az, col, c=10*np.log10(nmse_time.cpu()), cmap='coolwarm',vmax=1,vmin=-15)
 sc1 = ax1.scatter(az, col, c=10*np.log10(sarita_nmse), cmap='coolwarm',vmax=1,vmin=-15)
-
 azimuth_sampled,colatitude_sampled,_ = ut.cart2sph((data_handler.INPUT_SAMPLED[:,0],data_handler.INPUT_SAMPLED[:,1],
                                                        data_handler.INPUT_SAMPLED[:,2]))
 microphone_positions = np.column_stack((azimuth_sampled, colatitude_sampled))
-
 ax.scatter(microphone_positions[:,0],microphone_positions[:,1],facecolors='none',
             color='black', marker='o', label='Microphones')
 ax1.scatter(microphone_positions[:,0],microphone_positions[:,1],facecolors='none',
             color='black', marker='o', label='Microphones')
-
 azimuth_ticks = [0, np.pi / 2, np.pi, 2 * np.pi]
 colatitude_ticks = [0, np.pi / 2, np.pi]
 cbar = plt.colorbar(sc, label='NMSE (dB)')
 cbar1 = plt.colorbar(sc1, label='NMSE (dB)')
-
 ax.set_xticks(azimuth_ticks)
 ax.set_yticks(colatitude_ticks)
 ax1.set_xticks(azimuth_ticks)
@@ -120,15 +111,14 @@ ax.set_xticklabels(['0', 'π/2', 'π', '2π'])
 ax.set_yticklabels(['0', 'π/2', 'π'])
 ax1.set_xticklabels(['0', 'π/2', 'π', '2π'])
 ax1.set_yticklabels(['0', 'π/2', 'π'])
-
 plt.show()
 
 # Compute nmse for sh armonics
-
 sh_ground = process.spatFT(prevision_freq,gb.spherical_grid,4)
 mse = np.abs(sh_ground - gb.sh_lower) ** 2
 mse = np.sum(mse,axis=1)
 norm = np.sum(np.abs(gb.sh_lower) ** 2,axis=1)
+
 # Calculate NMSE in dB
 nmse_all_sh = mse / norm
 nmse_all_sh_db = 10*np.log10(nmse_all_sh)
